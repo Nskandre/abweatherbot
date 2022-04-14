dbb1d43fb069a4787efc1051cf3d8984d0bdf66c
Andrei <rinf@mail.ru>
2022-04-14 17:27:16 +0300

    Finish version abweatherbot. Added buttons

--- tg_bot.py
+++ tg_bot.py
@@ -1,21 +1,42 @@
+import telebot
+from telebot import types
+from settings import OPEN_WEATHER_TOKEN, TG_TOKEN
 import requests
 import datetime
-from settings import OPEN_WEATHER_TOKEN, TG_TOKEN
-from aiogram import Bot, types
-from aiogram.dispatcher import Dispatcher
-from aiogram.utils import executor
+from utils import create_button
 
-bot = Bot(token=TG_TOKEN)
-dp = Dispatcher(bot)
+# token = '5276898707:AAGRP_TsLPKpk30AK5VoDHnMb-TesKehMZU'
+bot = telebot.TeleBot(TG_TOKEN, parse_mode=None)
+
+BTN_MSK = create_button('msk', 'Москва')
+BTN_SPB = create_button('spb', 'Санкт-Петербург')
+BTN_EKB = create_button('ekb', 'Екатеринбург')
+# BTN_OTHER = create_button('other', 'Другой город')
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton('Москва') # callback_data='Moscow')
+    btn2 = types.KeyboardButton('Санкт-Петербург') #, 'Saint Petersburg')
+    btn3 = types.KeyboardButton('Екатеринбург') #, 'Ekaterinburg')
+    # btn4 = types.KeyboardButton('Другой город')  # , 'Other')
+    markup.add(btn1, btn2, btn3) #, btn4)
+
+    bot.reply_to(message, "Привет! Выбери город или напиши его название и получи сводку погоды!", reply_markup=markup)
 
 
-@dp.message_handler(commands=["start"])
-async def start_command(message: types.Message):
-    await message.reply("Напиши название города и я пришлю тебя сводку погоды!")
+@bot.message_handler(content_types=['text'])
+def send_message(message):
+    if message.text == BTN_MSK['text']:
+        city = 'Moscow'
+    elif message.text == BTN_SPB['text']:
+        city = 'Saint Petersburg'
+    elif message.text == BTN_EKB['text']:
+        city = 'Ekaterinburg'
+    else:
+        city = message.text
 
 
-@dp.message_handler()
-async def get_weather(message: types.Message):
     code_to_smile = {
         'Clear': 'Ясно \U00002600',
         'Clouds': 'Облачно \U00002601',
@@ -28,7 +49,7 @@ async def get_weather(message: types.Message):
 
     try:
         r = requests.get(
-            f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={OPEN_WEATHER_TOKEN}&units=metric'
+            f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_TOKEN}&units=metric'
         )
         data = r.json()
 
@@ -48,22 +69,16 @@ async def get_weather(message: types.Message):
         else:
             wd = 'Посмотри в окно, не пойму, что там за погода!'
 
-        await message.reply(f'''{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} Погода в {user_city}: 
-        {wd}
-        Температура: {temp} °C
-        Ощущается как: {feels_like} °C
-        Скорость ветра {wind} м/c
-        Влажность {humidity} %
-        Давление {round(pressure / 1.33322387415)} мм.рт.ст
-        Восход {sunrise} 
-        Закат {sunset}
-        Продолжительность дня {length} 
-
-        Отличного Вам дня!!!''')
+        bot.send_message(message.chat.id, f'''{datetime.datetime.now().strftime('%Y-%m-%d %H:%M')} Погода в {user_city}: 
+            {wd}
+            Температура: {temp} °C
+            Ощущается как: {feels_like} °C
+            Скорость ветра {wind} м/c
+            Влажность {humidity} %
+            Давление {round(pressure / 1.33322387415)} мм.рт.ст
+            Восход {sunrise} 
+            Закат {sunset}
+            Продолжительность дня {length}''')
 
     except:
-        await message.reply('\U00002620 Проверьте название города \U00002620')
-
-
-if __name__ == '__main__':
-    executor.start_polling(dp)
+        bot.send_message(message.chat.id, '\U00002620 Проверьте название города \U00002620')
